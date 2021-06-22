2f41197564d7c7f8f48bcc5563c672d3977a924c
hryder <r37he@edu.uwaterloo.ca>
2021-06-22 11:45:03 -0400

    tailwind

--- tailwindCSS/src/SearchParams.js
+++ tailwindCSS/src/SearchParams.js
@@ -27,25 +27,30 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="search-params">
+    <div
+      className="my-0 mx-auto w-11/12"
+    >
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center divide-y dicvide-gray-900"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
         }}
       >
-        <label htmlFor="location">
+        <label htmlFor="location"  className="search-label">
           Location
           <input
+            className="search-control"
             id="location"
             value={location}
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
+        <label htmlFor="animal"  className="search-label">
           Animal
           <select
+            className="search-control"
             id="animal"
             value={animal}
             onChange={(e) => updateAnimal(e.target.value)}
@@ -59,9 +64,10 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label htmlFor="breed">
+        <label htmlFor="breed" className="search-label">
           Breed
           <select
+            className="search-control disabled:opacity-50"
             disabled={!breeds.length}
             id="breed"
             value={breed}
@@ -76,9 +82,10 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label htmlFor="theme">
+        <label htmlFor="theme"  className="search-label">
           Theme
           <select
+            className="search-control"
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
@@ -89,7 +96,10 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button 
+          style={{ backgroundColor: theme }} 
+          className="rounded px--6 py-2 text-white hover:opacity-50 border-none"
+        >Submit</button>
       </form>
       <Results pets={pets} />
     </div>
